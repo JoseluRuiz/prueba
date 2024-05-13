c79a69c1da031b23ea2e0c980f39cf7ac404620b
idlir <idlir@LAPTOP-7CD8ULDA>
2024-05-13 18:15:00 +0200

    Primer commit

---
+++ src/firstPackage/Main.java
@@ -0,0 +1,16 @@
+package firstPackage;
+
+public class Main 
+{
+	public static void main (String [] args) 
+	{
+		int num = 0;
+		
+		for (int i = 0; i < 50; i++) 
+		{
+			num += 2*i;
+		}
+		
+		System.out.println("Number: "+num);
+	}
+}
